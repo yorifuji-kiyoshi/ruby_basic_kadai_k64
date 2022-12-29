6415a2e2f6dc35c6877db4e4d0db92e49e6b0358
Replit user <>
2022-12-29 06:33:55 +0000

    kadai

---
+++ kadai3.rb
@@ -0,0 +1,18 @@
+# num = Integer(gets.chomp)
+print "数字を入力してください : "
+num = gets.chomp
+x = Integer(num) rescue false
+
+if x
+  if x % 2 == 0 && x % 7 != 0
+    puts "2の倍数です"
+  elsif x % 7 == 0 && x % 2 != 0
+    puts "7の倍数です"
+  elsif x % 7 == 0 && x % 2 == 0
+    puts "14の倍数です"
+  else 
+    puts "2の倍数でも7の倍数でもありません"
+  end
+else
+  puts "整数ではありません"
+end
